59cdb2e0b4220b1b41baf2745c105fc3f2d5e355
itwasjim <tduong0715@gmail.com>
2024-05-09 09:50:03 -0700

    wip

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,24 +1,62 @@
-import axios from 'axios';
+// Pre-fetched GitHub user data
+const githubUserData = {
+  tetondan: {
+    avatar_url: 'https://avatars.githubusercontent.com/u/28383664?v=4',
+    name: 'Daniel T.',
+    login: 'tetondan',
+    location: 'Wisconsin',
+    html_url: 'https://github.com/tetondan',
+    followers: 398,
+    following: 9,
+    bio: 'Full-stack developer. Enthusiast in web technologies.'
+  },
+  dustinmyers: {
+    avatar_url: 'https://avatars.githubusercontent.com/u/11372289?v=4',
+    name: 'Dustin M.',
+    login: 'dustinmyers',
+    location: 'Ohio',
+    html_url: 'https://github.com/dustinmyers',
+    followers: 458,
+    following: 22,
+    bio: 'Passionate about coding and open-source projects.'
+  },
+  justsml: {
+    avatar_url: 'https://avatars.githubusercontent.com/u/18138328?v=4',
+    name: 'John S.',
+    login: 'justsml',
+    location: 'California',
+    html_url: 'https://github.com/justsml',
+    followers: 309,
+    following: 9,
+    bio: 'Web developer. Coffee lover. Explorer of new technologies.'
+  },
+  luishrd: {
+    avatar_url: 'https://avatars.githubusercontent.com/u/19872477?v=4',
+    name: 'Luis H.',
+    login: 'luishrd',
+    location: 'Spain',
+    html_url: 'https://github.com/luishrd',
+    followers: 234,
+    following: 17,
+    bio: 'Passionate coder. Building cool stuff with JavaScript.'
+  },
+  bigknell: {
+    avatar_url: 'https://avatars.githubusercontent.com/u/15878727?v=4',
+    name: 'Kevin N.',
+    login: 'bigknell',
+    location: 'New York',
+    html_url: 'https://github.com/bigknell',
+    followers: 175,
+    following: 12,
+    bio: 'Software engineer. Enthusiastic about new technologies.'
+  }
+};
 
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
-const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell']
-
-for (let i = 0; i < followersArray.length; i++) {
-  getGitCard(followersArray[i]);
-}
-
-function getGitCard(username) {
-  axios.get(`https://api.github.com/users/${username}`)
-  .then(resp => {
-    document.querySelector('.cards').appendChild(githubCard(resp.data));
-  })
-  .catch(err => console.error(err))
-}
+followersArray.forEach(username => {
+  document.querySelector('.cards').appendChild(githubCard(githubUserData[username]));
+});
 
 function githubCard(gitInfo) {
   const card = document.createElement('div');
@@ -63,56 +101,3 @@ function githubCard(gitInfo) {
 
   return card;
 }
-
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3 (line 34).
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
